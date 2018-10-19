41a640c1b6a2faca47c00044ff3f9ff45e6c1588
Enrique Tun <enrique.tun@adxpace.com>
2018-10-18 19:34:19 -0500

    cree el primer template

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.posts_home, name="index"),
+    path('', views.posts_lista, name="index"),
+    path('crear/', views.posts_crear),
+    path('detalle', views.posts_detalles),
+    path('actualizar', views.posts_actualizar),
+    path('eliminar', views.posts_eliminar)
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def posts_home(request):
-    return HttpResponse("<h1>Hola a todos</h1>")
+def posts_lista(request):
+    return render(request, "index.html")
+
+def posts_crear(request):
+    return HttpResponse("<h1>Hola a crear</h1>")
+
+def posts_detalles(request):
+    return HttpResponse("<h1>Hola a detalles</h1>")
+
+def posts_actualizar(request):
+    return HttpResponse("<h1>Hola a actualizar</h1>")
+
+def posts_eliminar(request):
+    return HttpResponse("<h1>Hola a eliminar</h1>")
